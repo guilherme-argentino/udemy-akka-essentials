b8bf2d8e4f13e76679ac77044ea696a63c1690ee
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-22 10:37:07 -0300

    12. Changing Actor Behavior

---
+++ src/main/scala/part2actors/ChangingActorBehavior.scala
@@ -0,0 +1,97 @@
+package com.github.argentino.udemy.akka
+package part2actors
+
+import part2actors.ChangingActorBehavior.Mom.MomStart
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object ChangingActorBehavior extends App {
+
+  object FussyKid {
+
+    case object KidAccept
+
+    case object KidReject
+
+    val HAPPY = "happy"
+    val SAD = "sad"
+  }
+
+  class FussyKid extends Actor {
+
+    import FussyKid._
+    import Mom._
+
+    //internal state of the kid
+    var state: String = HAPPY
+
+    override def receive: Receive = {
+      case Food(VEGETABLE) => state = SAD
+      case Food(CHOCOLATE) => state = HAPPY
+      case Ask(_) =>
+        if (state == HAPPY) sender() ! KidAccept
+        else sender() ! KidReject
+    }
+  }
+
+  class StatelessFussyKid extends Actor {
+
+    import FussyKid._
+    import Mom._
+
+    override def receive: Receive = happyReceive
+
+    def happyReceive: Receive = {
+      case Food(VEGETABLE) => context.become(sadReceive) // change my receive handler to sadReceive
+      case Food(CHOCOLATE) => // stay happy
+      case Ask(_) => sender() ! KidAccept
+    }
+
+    def sadReceive: Receive = {
+      case Food(VEGETABLE) => // stay sad
+      case Food(CHOCOLATE) => context.become(happyReceive) // change my receive handler to happyReceive
+      case Ask(_) => sender() ! KidReject
+    }
+  }
+
+  object Mom {
+
+    case class MomStart(kidRef: ActorRef)
+
+    case class Food(food: String)
+
+    case class Ask(message: String) // do you want to play?
+    val VEGETABLE = "veggies"
+    val CHOCOLATE = "chocolate"
+  }
+
+  class Mom extends Actor {
+
+    import FussyKid._
+    import Mom._
+
+    override def receive: Receive = {
+      case MomStart(kidRef) =>
+        // test our interaction
+        kidRef ! Food(VEGETABLE)
+        kidRef ! Ask("do you want to play?")
+      case KidAccept => println("Yay, my kid is happy!")
+      case KidReject => println("My kid is sad, but as he's healthy!")
+    }
+  }
+
+  val system = ActorSystem("changingActorBehaviorDemo")
+  val fussyKid = system.actorOf(Props[FussyKid])
+  val statelessFussyKid = system.actorOf(Props[StatelessFussyKid])
+  val mom = system.actorOf(Props[Mom])
+
+  mom ! MomStart(statelessFussyKid)
+
+  /*
+    mom receives MomStart
+      kid receives Food(veg) -> kid will change the handler to sadReceive
+      kid receives Ask(play?) -> kid replies with sadReceive handler =>
+    mom receives KidReject
+   */
+
+}
