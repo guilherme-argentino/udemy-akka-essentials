a067a9f313e805a648a91e492baa039289879517
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-22 21:47:37 -0300

    15. Child Actors (creating first child) closing over

--- src/main/scala/part2actors/ChildActors.scala
+++ src/main/scala/part2actors/ChildActors.scala
@@ -2,6 +2,7 @@ package com.github.argentino.udemy.akka
 package part2actors
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import com.github.argentino.udemy.akka.part2actors.ChildActors.CreditCard.{AttachToAccount, CheckStatus}
 
 object ChildActors extends App {
 
@@ -40,4 +41,85 @@ object ChildActors extends App {
   parent ! CreateChild("child")
   parent ! TellChild("hey Kid!")
 
+  // actor hierarchies
+  // parent -> child -> grandChild
+  //        -> child2 ->
+
+  /*
+    Guardian actors (top-level)
+    - /system = system guardian
+    - /user = user-level guardian
+    - / = the root guardian
+   */
+
+  /**
+   * Actor selection
+   */
+  val childSelection = system.actorSelection("/user/parent/child")
+  childSelection ! "I found you!"
+
+  /**
+   * Danger!
+   *
+   * NEVER PASS MUTABLE ACTOR STATE, OR THE 'THIS' REFERENCE, TO CHILD ACTORS.
+   *
+   * NEVER IN YOUR LIFE.
+   */
+
+  object NaiveBankAccount {
+    case class Deposit(amount: Int)
+    case class Withdraw(amount: Int)
+    case object InitializeAccount
+  }
+  class NaiveBankAccount extends Actor {
+    import NaiveBankAccount._
+    import CreditCard._
+
+    var amount = 0
+    override def receive: Receive = {
+      case InitializeAccount =>
+        val creditCardRef = context.actorOf(Props[CreditCard], "card")
+        creditCardRef ! AttachToAccount(this) // !!
+      case Deposit(funds) => deposit(funds)
+      case Withdraw(funds) => withdraw(funds)
+    }
+
+    def deposit(funds: Int) = {
+      println(s"${self.path} depositing $funds on top of $amount")
+      amount += funds
+    }
+    def withdraw(funds: Int) = {
+      println(s"${self.path} withdrawing $funds from $amount")
+      amount -= funds
+    }
+  }
+
+  object CreditCard {
+    case class AttachToAccount(bankAccount: NaiveBankAccount) // !!
+    case object CheckStatus
+  }
+  class CreditCard extends Actor {
+
+    override def receive: Receive = {
+      case AttachToAccount(account) => context.become(attachedTo(account))
+    }
+
+    def attachedTo(account: NaiveBankAccount): Receive = {
+      case CheckStatus =>
+        println(s"${self.path} your message has been processed.")
+        // benign
+        account.withdraw(1) // because I can
+    }
+  }
+
+  import NaiveBankAccount._
+  import CreditCard._
+
+  val bankAccountRef = system.actorOf(Props[NaiveBankAccount], "account")
+  bankAccountRef ! InitializeAccount
+  bankAccountRef ! Deposit(100)
+
+  Thread.sleep(500)
+  val ccSelection = system.actorSelection("/user/account/card")
+  ccSelection ! CheckStatus
 }
