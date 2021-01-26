e650f35c3d24d2ced2efb67c661e9c4aabd3e1dc
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-26 20:56:55 -0300

    20. TestProbes - final

--- src/main/scala/part3testing/TestProbeSpec.scala
+++ src/main/scala/part3testing/TestProbeSpec.scala
@@ -25,8 +25,41 @@ class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec"))
       master ! Register(slave.ref)
       expectMsg(RegistrationAck)
     }
-  }
 
+    "send the work to the slave actor" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+
+      val workloadString = "I love Akka"
+      master ! Work(workloadString)
+
+      // the interaction between the master and the slave actor
+      slave.expectMsg(SlaveWork(workloadString, testActor))
+      slave.reply(WorkCompleted(3, testActor))
+
+      expectMsg(Report(3)) // testActor receives the Report(3)
+    }
+
+    "aggregate data correctly" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+
+      val workloadString = "I love Akka"
+      master ! Work(workloadString)
+      master ! Work(workloadString)
+
+      // in the meantime I don't have a slave actor
+      slave.receiveWhile() {
+        case SlaveWork(`workloadString`, `testActor`) => slave.reply(WorkCompleted(3, testActor))
+      }
+      expectMsg(Report(3))
+      expectMsg(Report(6))
+    }
+  }
 }
 
 object TestProbeSpec {
