1b6a2c0ea1e472cc96f52d93a65652acffb7eda3
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-26 20:44:20 -0300

    20. TestProbes - register slave

---
+++ src/main/scala/part3testing/TestProbeSpec.scala
@@ -0,0 +1,71 @@
+package com.github.argentino.udemy.akka
+package part3testing
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import TestProbeSpec._
+
+  "A master actor" should {
+    "register a slave" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+    }
+  }
+
+}
+
+object TestProbeSpec {
+  // scenario
+  /*
+    word counting actor hierachy master-slave
+
+    send some work to the master
+      - master sends the slave the piece of work
+      - slave processes the work and replies to master
+      - master aggregates the result
+    master sends the total count to the original requester
+   */
+
+  case class Work(text: String)
+  case class SlaveWork(text: String, originalRequester: ActorRef)
+  case class WorkCompleted(count: Int, originalRequester: ActorRef)
+  case class Register(slaveRef: ActorRef)
+  case object RegistrationAck
+  case class Report(totalCount: Int)
+
+  class Master extends Actor {
+
+    override def receive: Receive = {
+      case Register(slaveRef) =>
+        sender() ! RegistrationAck
+        context.become(online(slaveRef, 0))
+      case _ => // ignore
+    }
+
+    def online(slaveRef: ActorRef, totalWordCount: Int): Receive = {
+      case Work(text) => slaveRef ! SlaveWork(text, sender())
+      case WorkCompleted(count, originalRequester) =>
+        val newTotalWordCount = totalWordCount + count
+        originalRequester ! Report(newTotalWordCount)
+        context.become(online(slaveRef, newTotalWordCount))
+    }
+  }
+
+  // class Slave extends Actor ....
+
+}
