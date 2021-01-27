d68355569be2586a02ef108a271df97a88056036
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-26 22:33:47 -0300

    21. Timed Assertions - first within

--- src/main/scala/part3testing/TimedAssertionsSpec.scala
+++ src/main/scala/part3testing/TimedAssertionsSpec.scala
@@ -1,9 +1,12 @@
 package com.github.argentino.udemy.akka
 package part3testing
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import scala.concurrent.duration._
+
+import scala.util.Random
 
 class TimedAssertionsSpec extends TestKit(ActorSystem("TimedAssertionsSpec"))
   with ImplicitSender
@@ -17,8 +20,36 @@ class TimedAssertionsSpec extends TestKit(ActorSystem("TimedAssertionsSpec"))
 
   import TimedAssertionsSpec._
 
+  "A worker actor" should {
+    val workerActor = system.actorOf(Props[WorkerActor])
+
+    "reply with the meaning of life in a timely manner" in {
+      within(500 millis, 1 second) {
+        workerActor ! "work"
+        expectMsg(WorkResult(42))
+      }
+    }
+  }
+
 }
 
 object TimedAssertionsSpec {
-  // testing scenario
+
+  case class WorkResult(result: Int)
+
+  class WorkerActor extends Actor {
+    override def receive: Receive = {
+      case "work" =>
+        // long computation
+        Thread.sleep(500)
+        sender ! WorkResult(42)
+
+      case "workSequence" =>
+        val r = new Random()
+        for (_ <- 1 to 10) {
+          Thread.sleep(r.nextInt(50))
+          sender() ! WorkResult(1)
+        }
+    }
+  }
 }
