0d2d402b82d90e2e688d23879fcddfc1d2c13565
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-27 13:20:00 -0300

    27. The Backoff Supervision Pattern - initial

---
+++ src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
@@ -0,0 +1,86 @@
+package com.github.argentino.udemy.akka
+package part4faulttolerance
+
+import akka.actor.SupervisorStrategy.Stop
+import akka.actor.{Actor, ActorLogging, ActorSystem, OneForOneStrategy, Props}
+import akka.pattern.{Backoff, BackoffSupervisor}
+
+import java.io.File
+import scala.concurrent.duration._
+import scala.io.Source
+
+object BackoffSupervisorPattern extends App {
+
+  case object ReadFile
+  class FileBasedPersistentActor extends Actor with ActorLogging {
+    var dataSource: Source = null
+
+    override def preStart(): Unit = log.info("Persistent actor starting")
+
+    override def postStop(): Unit = log.warning("Persistent actor has stopped")
+
+    override def preRestart(reason: Throwable, message: Option[Any]): Unit = log.warning("Persistent actor restarting")
+
+    override def receive: Receive = {
+      case ReadFile =>
+        if (dataSource == null)
+          dataSource = Source.fromFile(new File("src/main/resources/testfiles/important_data.txt"))
+        log.info("I've just read some IMPORTANT data: " + dataSource.getLines().toList)
+    }
+  }
+
+  val system = ActorSystem("BackoffSupervisorDemo")
+//  val simpleActor = system.actorOf(Props[FileBasedPersistentActor], "simpleActor")
+//  simpleActor ! ReadFile
+
+  val simpleSupervisorProps = BackoffSupervisor.props(
+    Backoff.onFailure(
+      Props[FileBasedPersistentActor],
+      "simpleBackoffActor",
+      3 seconds, // then 6s, 12s, 24s
+      30 seconds,
+      0.2
+    )
+  )
+
+//  val simpleBackoffSupervisor = system.actorOf(simpleSupervisorProps, "simpleSupervisor")
+//  simpleBackoffSupervisor ! ReadFile
+
+  /*
+    simpleSupervisor
+      - child called simpleBackoffActor (props of type FileBasedPersistentActor)
+      - supervision strategy is the default one (restarting on everything)
+        - first attempt after 3 seconds
+        - next attempt is 2x the previous attempt
+  */
+
+  val stopSupervisorProps = BackoffSupervisor.props(
+    Backoff.onStop(
+      Props[FileBasedPersistentActor],
+      "stopBackoffActor",
+      3 seconds,
+      30 seconds,
+      0.2
+    ).withSupervisorStrategy(
+      OneForOneStrategy() {
+        case _ => Stop
+      }
+    )
+  )
+
+//  val stopSupervisor = system.actorOf(stopSupervisorProps, "stopSupervisor")
+//  stopSupervisor ! ReadFile
+
+  class EagerFBPActor extends FileBasedPersistentActor {
+    override def preStart(): Unit = {
+      log.info("Eager actor starting")
+      dataSource = Source.fromFile(new File("src/main/resources/testfiles/important_data.txt"))
+    }
+  }
+
+  val eagerActor = system.actorOf(Props[EagerFBPActor])
+
+  // ActorInitializationException => STOP
+
+
+}
