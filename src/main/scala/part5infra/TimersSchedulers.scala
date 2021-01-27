4597d8296bd7483d68dc9bc97dc17cc7aa99ff43
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-27 13:37:08 -0300

    28. Schedulers and Timers - explicit dispatcher

---
+++ src/main/scala/part5infra/TimersSchedulers.scala
@@ -0,0 +1,25 @@
+package com.github.argentino.udemy.akka
+package part5infra
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+
+import scala.concurrent.duration._
+
+object TimersSchedulers extends App {
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
+
+  val system = ActorSystem("SchedulersTimersDemo")
+  val simpleActor = system.actorOf(Props[SimpleActor])
+
+  system.log.info("Scheduling reminder for simpleActor")
+
+  system.scheduler.scheduleOnce(1 second) {
+    simpleActor ! "reminder"
+  } (system.dispatcher)
+
+}
