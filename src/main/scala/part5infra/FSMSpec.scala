4290dc011c1be27cfd4c1cf3098ae46cf876f8b6
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-27 18:54:33 -0300

    34. (optional) Finite State Machines - State Machine

--- src/main/scala/part5infra/FSMSpec.scala
+++ src/main/scala/part5infra/FSMSpec.scala
@@ -1,12 +1,12 @@
 package com.github.argentino.udemy.akka
 package part5infra
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging, ActorSystem}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
 class FSMSpec extends TestKit(ActorSystem("FSMSpec"))
-  with ImplicitSender with WordSpecLike with BeforeAndAfterAll with OneInstancePerTest {
+  with ImplicitSender with WordSpecLike with BeforeAndAfterAll {
 
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
@@ -17,4 +17,22 @@ class FSMSpec extends TestKit(ActorSystem("FSMSpec"))
 
 object FSMSpec {
 
+  /*
+    Vending machine
+   */
+
+  case class Initialize(inventory: Map[String, Int], prices: Map[String, Int])
+  case class RequestProduct(product: String)
+
+  case class Instruction(instruction: String) // message the VM will show on its "screen"
+  case class ReceiveMoney(amount: Int)
+  case class Deliver(product: String)
+  case class GiveBackChange(amount: Int)
+
+  case class VendingError(reason: String)
+  case object ReceiveMoneyTimeout
+
+  class VendingMachine extends Actor with ActorLogging {
+    override def receive: Receive = ???
+  }
 }
