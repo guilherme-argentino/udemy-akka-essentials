946e57a57e304565260e10d432e2ad44bc875a4f
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-27 14:12:29 -0300

    28. Schedulers and Timers - Exercise solution

--- src/main/scala/part5infra/TimersSchedulers.scala
+++ src/main/scala/part5infra/TimersSchedulers.scala
@@ -39,4 +39,35 @@ object TimersSchedulers extends App {
    * - if the time window expires, the actor will stop itself
    * - if you send another message, the time window is reset
    */
+
+
+  class SelfClosingActor extends Actor with ActorLogging {
+    var schedule = createTimeoutWindow()
+
+    def createTimeoutWindow(): Cancellable = {
+      context.system.scheduler.scheduleOnce(1 second) {
+        self ! "timeout"
+      }
+    }
+
+    override def receive: Receive = {
+      case "timeout" =>
+        log.info("Stopping myself")
+        context.stop(self)
+      case message =>
+        log.info(s"Received $message, staying alive")
+        schedule.cancel()
+        schedule = createTimeoutWindow()
+    }
+  }
+
+    val selfClosingActor = system.actorOf(Props[SelfClosingActor], "selfClosingActor")
+    system.scheduler.scheduleOnce(250 millis) {
+      selfClosingActor ! "ping"
+    }
+
+    system.scheduler.scheduleOnce(2 seconds) {
+      system.log.info("sending pong to the self-closing actor")
+      selfClosingActor ! "pong"
+    }
 }
