3af11bd4fad699c0f70b9282dd32dc7625755758
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-27 18:28:30 -0300

    33. The Ask Pattern - six tests passed

--- src/main/scala/part6patterns/AskSpec.scala
+++ src/main/scala/part6patterns/AskSpec.scala
@@ -12,6 +12,7 @@ import scala.util.{Failure, Success}
 
 // step 1 - import the ask pattern
 import akka.pattern.ask
+import akka.pattern.pipe
 
 class AskSpec extends TestKit(ActorSystem("AskSpec"))
   with ImplicitSender with WordSpecLike with BeforeAndAfterAll {
@@ -36,6 +37,37 @@ class AskSpec extends TestKit(ActorSystem("AskSpec"))
       authManager ! RegisterUser("daniel", "rtjvm")
       authManager ! Authenticate("daniel", "iloveakka")
       expectMsg(AuthFailure(AUTH_FAILURE_PASSWORD_INCORRECT))
+    }
+
+    "successfully authenticate a registered user" in {
+      val authManager = system.actorOf(Props[AuthManager])
+      authManager ! RegisterUser("daniel", "rtjvm")
+      authManager ! Authenticate("daniel", "rtjvm")
+      expectMsg(AuthSuccess)
+    }
+  }
+
+  "A piped authenticator" should {
+    import AuthManager._
+
+    "fail to authenticate a non-registered user" in {
+      val authManager = system.actorOf(Props[PipedAuthManager])
+      authManager ! Authenticate("daniel", "rtjvm")
+      expectMsg(AuthFailure(AUTH_FAILURE_NOT_FOUND))
+    }
+
+    "fail to authenticate if invalid password" in {
+      val authManager = system.actorOf(Props[PipedAuthManager])
+      authManager ! RegisterUser("daniel", "rtjvm")
+      authManager ! Authenticate("daniel", "iloveakka")
+      expectMsg(AuthFailure(AUTH_FAILURE_PASSWORD_INCORRECT))
+    }
+
+    "successfully authenticate a registered user" in {
+      val authManager = system.actorOf(Props[PipedAuthManager])
+      authManager ! RegisterUser("daniel", "rtjvm")
+      authManager ! Authenticate("daniel", "rtjvm")
+      expectMsg(AuthSuccess)
     }
   }
 
@@ -81,26 +113,54 @@ object AskSpec {
     implicit val timeout: Timeout = Timeout(1 second)
     implicit val executionContext: ExecutionContext = context.dispatcher
 
-    private val authDb = context.actorOf(Props[KVActor])
+    protected val authDb = context.actorOf(Props[KVActor])
 
     override def receive: Receive = {
       case RegisterUser(username, password) => authDb ! Write(username, password)
-      case Authenticate(username, password) =>
-        val originalSender = sender()
-        // step 3 - ask the actor
-        val future = authDb ? Read(username)
-        // step 4 - handle the future for e.g. with onComplete
-        future.onComplete {
-          // step 5 most important
-          // NEVER CALL METHODS ON THE ACTOR INSTANCE OR ACCESS MUTABLE STATE IN ONCOMPLETE
-          // avoid closing over the actor instance or mutable state
-          case Success(None) => originalSender ! AuthFailure(AUTH_FAILURE_NOT_FOUND)
-          case Success(Some(dbPassword)) =>
-            if (dbPassword == password) originalSender ! AuthSuccess
-            else originalSender ! AuthFailure(AUTH_FAILURE_PASSWORD_INCORRECT)
-          case Failure(_) => originalSender ! AuthFailure(AUTH_FAILURE_SYSTEM)
-        }
+      case Authenticate(username, password) => handleAuthentication(username, password)
+    }
+
+    def handleAuthentication(username: String, password: String) = {
+      val originalSender = sender()
+      // step 3 - ask the actor
+      val future = authDb ? Read(username)
+      // step 4 - handle the future for e.g. with onComplete
+      future.onComplete {
+        // step 5 most important
+        // NEVER CALL METHODS ON THE ACTOR INSTANCE OR ACCESS MUTABLE STATE IN ONCOMPLETE.
+        // avoid closing over the actor instance or mutable state
+        case Success(None) => originalSender ! AuthFailure(AUTH_FAILURE_NOT_FOUND)
+        case Success(Some(dbPassword)) =>
+          if (dbPassword == password) originalSender ! AuthSuccess
+          else originalSender ! AuthFailure(AUTH_FAILURE_PASSWORD_INCORRECT)
+        case Failure(_) => originalSender ! AuthFailure(AUTH_FAILURE_SYSTEM)
+      }
     }
   }
+
+  class PipedAuthManager extends AuthManager {
+
+    import AuthManager._
+
+    override def handleAuthentication(username: String, password: String): Unit = {
+      // step 3 - ask the actor
+      val future = authDb ? Read(username) // Future[Any]
+      // step 4 - process the future until you get the responses you will send back
+      val passwordFuture = future.mapTo[Option[String]] // Future[Option[String]]
+      val responseFuture = passwordFuture.map {
+        case None => AuthFailure(AUTH_FAILURE_NOT_FOUND)
+        case Some(dbPassword) =>
+          if (dbPassword == password) AuthSuccess
+          else AuthFailure(AUTH_FAILURE_PASSWORD_INCORRECT)
+      } // Future[Any] - will be completed with the response I will send back
+
+      // step 5 - pipe the resulting future to the actor you want to send the result to
+      /*
+        When the future completes, send the response to the actor ref in the arg list.
+       */
+      responseFuture.pipeTo(sender())
+    }
+  }
+
 
 }
