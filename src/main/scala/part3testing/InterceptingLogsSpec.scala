4cb3b6b55cf5238fc0f7d0f7742d8a02699f8ebf
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-26 23:28:17 -0300

    22. Intercepting Logs - can´t find error

--- src/main/scala/part3testing/InterceptingLogsSpec.scala
+++ src/main/scala/part3testing/InterceptingLogsSpec.scala
@@ -1,11 +1,13 @@
 package com.github.argentino.udemy.akka
 package part3testing
 
-import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
-class InterceptingLogsSpec extends TestKit(ActorSystem("InterceptingLogsSpec"))
+class InterceptingLogsSpec extends TestKit(
+  ActorSystem("InterceptingLogsSpec", ConfigFactory.load().getConfig("interceptingLogMessages")))
   with ImplicitSender
   with WordSpecLike
   with BeforeAndAfterAll {
@@ -14,8 +16,89 @@ class InterceptingLogsSpec extends TestKit(ActorSystem("InterceptingLogsSpec"))
     super.afterAll()
     TestKit.shutdownActorSystem(system)
   }
+
+  import InterceptingLogsSpec._
+
+  val item = "Rock the JVM Akka course"
+  val creditCard = "1234-1234-1234-1234"
+  val invalidCreditCard = "0000-0000-0000-0000"
+  "A checkout flow" should {
+    "correctly log the dispatch of an order" in {
+      EventFilter.info(pattern = s"Order [0-9]+ for item $item has ben dispatched.", occurrences = 1) intercept {
+        // out test code
+        val checkoutRef = system.actorOf(Props[CheckoutActor])
+        checkoutRef ! Checkout(item, creditCard)
+      }
+    }
+
+    "freak out if the payment is denied" in {
+      EventFilter[RuntimeException](occurrences = 1) intercept {
+        // out test code
+        val checkoutRef = system.actorOf(Props[CheckoutActor])
+        checkoutRef ! Checkout(item, invalidCreditCard)
+      }
+    }
+  }
 }
 
 object InterceptingLogsSpec {
+
+  case class Checkout(item: String, creditCard: String)
+
+  case class AuthorizeCard(creditCard: String)
+
+  case object PaymentAccepted
+
+  case object PaymentDenied
+
+  case class DispatchOrder(item: String)
+
+  case object OrderConfirmed
+
+  class CheckoutActor extends Actor {
+    private val paymentManager = context.actorOf(Props[PaymentManager])
+    private val fulfillmentManager = context.actorOf(Props[FulfillmentManager])
+
+    override def receive: Receive = awaitingCheckout
+
+    def awaitingCheckout: Receive = {
+      case Checkout(item, card) =>
+        paymentManager ! AuthorizeCard(card)
+        context.become(pendingPayment(item))
+    }
+
+    def pendingPayment(item: String): Receive = {
+      case PaymentAccepted =>
+        fulfillmentManager ! DispatchOrder(item)
+        context.become(pendingFulfillment(item))
+      case PaymentDenied =>
+        throw new RuntimeException("I can't handle this anymore")
+    }
+
+    def pendingFulfillment(item: String): Receive = {
+      case OrderConfirmed => context.become(awaitingCheckout)
+    }
+  }
+
+  class PaymentManager extends Actor {
+    override def receive: Receive = {
+      case AuthorizeCard(card) =>
+        if (card.startsWith("0")) sender() ! PaymentDenied
+        else {
+//          Thread.sleep(4000)
+          sender() ! PaymentAccepted
+        }
+    }
+  }
+
+  class FulfillmentManager extends Actor with ActorLogging {
+    var orderId = 43
+    override def receive: Receive = {
+      case DispatchOrder(item: String) =>
+        orderId += 1
+        log.info(s"Order $orderId for item $item has been dispatched.")
+        sender() ! OrderConfirmed
+    }
+  }
 
 }
