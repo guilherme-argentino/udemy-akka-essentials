76e371a4d7bcd639f8dd65b69a47ce9b8c65d028
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-27 15:57:42 -0300

    29. Routers - final

--- src/main/scala/part5infra/Routers.scala
+++ src/main/scala/part5infra/Routers.scala
@@ -54,15 +54,15 @@ object Routers extends App {
    */
   // 2.1 programmatically (in code)
   val poolMaster = system.actorOf(RoundRobinPool(5).props(Props[Slave]), "simplePoolMaster")
-//  for (i <- 1 to 10) {
-//    poolMaster ! s"[$i] Hello from the world"
-//  }
+  //  for (i <- 1 to 10) {
+  //    poolMaster ! s"[$i] Hello from the world"
+  //  }
 
   // 2.2 from configuration
   val poolMaster2 = system.actorOf(FromConfig.props(Props[Slave]), "poolMaster2")
-//  for (i <- 1 to 10) {
-//    poolMaster2 ! s"[$i] Hello from the world"
-//  }
+  //  for (i <- 1 to 10) {
+  //    poolMaster2 ! s"[$i] Hello from the world"
+  //  }
 
   /**
    * Method #3 - router with actors created elsewhere
@@ -76,15 +76,15 @@ object Routers extends App {
 
   // 3.1 in the code
   val groupMaster = system.actorOf(RoundRobinGroup(slavePaths).props())
-//  for (i <- 1 to 10) {
-//    groupMaster ! s"[$i] Hello from the world"
-//  }
+  //  for (i <- 1 to 10) {
+  //    groupMaster ! s"[$i] Hello from the world"
+  //  }
 
   // 3.2 from configuration
   val groupMaster2 = system.actorOf(FromConfig.props(), "groupMaster2")
-//  for (i <- 1 to 10) {
-//    groupMaster2 ! s"[$i] Hello from the world"
-//  }
+  //  for (i <- 1 to 10) {
+  //    groupMaster2 ! s"[$i] Hello from the world"
+  //  }
 
   /**
    * Special messages
