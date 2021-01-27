6baaf159c1f922766ae86d55d87ff94636067012
Guilherme Luiz Argentino Silva <rrantz@gmail.com>
2021-01-27 16:53:23 -0300

    31. Mailboxes - with error

---
+++ src/main/scala/part5infra/Mailboxes.scala
@@ -0,0 +1,44 @@
+package com.github.argentino.udemy.akka
+package part5infra
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.dispatch.{PriorityGenerator, UnboundedPriorityMailbox}
+import com.typesafe.config.Config
+
+object Mailboxes extends App {
+
+  val system = ActorSystem("MailboxDemo")
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
+
+  /**
+   * Interesting case #1 - custom priority mailbox
+   * P0 -> most important
+   * P1
+   * P2
+   * P3
+   */
+
+  // step 1 - mailbox definition
+  class SupportTicketPriorityMailbox(settings: ActorSystem.Settings, config: Config)
+    extends UnboundedPriorityMailbox(
+      PriorityGenerator {
+        case message: String if message.startsWith("[P0]") => 0
+        case message: String if message.startsWith("[P1]") => 1
+        case message: String if message.startsWith("[P2]") => 2
+        case message: String if message.startsWith("[P3]") => 3
+        case _ => 4
+      })
+
+  // step 2 - make it known in the config
+  // step 3 - attach the dispatcher to an actor
+
+  val supportTicketLogger = system.actorOf(Props[SimpleActor].withDispatcher("support-ticket-dispatcher"))
+  supportTicketLogger ! "[P3] this thing would be nice to have"
+  supportTicketLogger ! "[P0] this needs to be solved NOW!"
+  supportTicketLogger ! "[P1] do this when you have the time"
+}
